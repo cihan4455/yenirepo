e17140aef539a613713ee35a45bfbbed85e28b0d
Cihan Karanfil <cihankaranfil44@hotmail.com>
2023-05-06 20:02:23 +0300

    version1

---
+++ src/main/java/class01.java
@@ -0,0 +1,13 @@
+public class class01 {
+
+    public static void main(String[] args) {
+
+        int x =1;
+        int y =2;
+        System.out.println(x+y);
+        System.out.println("versiyon 1 için ilemler tamam");
+
+
+    }
+
+}
