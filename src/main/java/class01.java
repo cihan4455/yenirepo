284c4a42f56663184979fc6a96a6ed8600c66fc4
Cihan Karanfil <cihankaranfil44@hotmail.com>
2023-05-06 20:21:29 +0300

    v3

--- src/main/java/class01.java
+++ src/main/java/class01.java
@@ -6,6 +6,12 @@ public class class01 {
         int y =2;
         System.out.println(x+y);
         System.out.println("versiyon 1 için ilemler tamam");
+        //version02 çalişmaları
+        int z=4;
+        System.out.println(x+y+z);
+        System.out.println("1v3 kod");
+
+
 
 
     }
